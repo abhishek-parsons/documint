14d8ca5e61d99f06788e10c6c0974858b4efac32
personal <abhishekkalia.productdesign@gmail.com>
2023-04-25 17:43:55 -0400

    Add animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,55 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Sections/Navbar";
 import Hero from "@/components/Sections/Hero";
 import Features from "@/components/Sections/Features";
 import Footer from "@/components/Sections/Footer";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="container">
-      <Navbar />
-      <Hero />
-      <Features />
+      <motion.div
+        className="box"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.2,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <Navbar />
+      </motion.div>
+
+      <motion.div
+        className="box"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.4,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <Hero />
+      </motion.div>
+
+      <motion.div
+        className="box"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.4,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <Features />
+      </motion.div>
       <Footer />
     </main>
   );
